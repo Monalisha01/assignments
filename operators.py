16313d79dd3fa81cb207743d77a7bbefb8d11bce
Monalisha01 <90247327+Monalisha01@users.noreply.github.com>
2021-09-19 23:25:39 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 18 12:49:17 2021
+
+@author: Asus
+"""
+
+#operators
+#arithmetic operator
+x=10
+y=5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y) 
+
+#assignment operator
+x+=3
+print(x)
+x-=3
+print(x)
+x*=3
+print(x)
+x/=3
+print(x)
+x%=3
+print(x)
+x**3
+print(x)
+x//3
+print(x)
+
+#comparision
+a=10
+b=9
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#logical
+print(a<5 and b>5)
+print(a<5 or b<10)
+print(not(a<5 and a<10))
+
+#identity
+print(a is b)
+print(a is not b)
+
+#membership
+t=["apple","banana","sea"]
+print("apple" in t)
+print("sea" not in t)
+
+
+
