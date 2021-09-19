a768174e15733e2654989216eb6852cd579d4bda
Monalisha01 <90247327+Monalisha01@users.noreply.github.com>
2021-09-19 23:26:41 +0530

    Add files via upload

---
+++ assingment.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 22:01:32 2021
+
+@author: Asus
+"""
+
+# 1. algorithm for findingx and y from the sequence: 10,1,8,3,6,5,4,7,x,y
+
+"""
+1. start: put n1=10 and n2=1
+2. n3=n1-2 and n4=n2+2
+3.n1=n3 and n2=n4
+4. end 
+"""
+
+
+#2. "end" in print function in python
+
+""" it is used to end a print statement with any character or string."""
+# 'to print welcome to python'
+print (" welcome to ",end="")
+print ("python",end=".")
+
+
